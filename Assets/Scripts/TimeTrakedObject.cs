e9595a0f4abe5a0068b733e05b6da464615858ed
Lyrimi <lyrimisin@gmail.com>
2026-03-20 10:03:22 +0100

    More time stuff
    Added basic traking

--- Assets/Scripts/TimeTrakedObject.cs
+++ Assets/Scripts/TimeTrakedObject.cs
@@ -1,11 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class TimeTrakedObject : MonoBehaviour
 {
+    List<Vector3> trackedPostion;
+    List<Quaternion> trackedRotation;
+    List<Vector3> trackedVelocity;
+    List<int> count;
+    int index = 0;
+
+
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        trackedPostion = new();
+        trackedRotation = new();
+        trackedVelocity = new();
+        count = new();
+        trackedPostion.Add(transform.position);
+        trackedRotation.Add(transform.rotation);
+        trackedVelocity.Add(rb.linearVelocity);
+        count.Add(1);
     }
     void Awake()
     {
@@ -13,9 +31,21 @@ public class TimeTrakedObject : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        if (trackedPostion[index] == transform.position && trackedRotation[index] == transform.rotation && trackedVelocity[index] == rb.linearVelocity)
+        {
+            //print("Same Object Skiping");
+            count[index]++;
+        }
+        else
+        {
+            //print("Updating Object");
+            trackedPostion.Add(transform.position);
+            trackedRotation.Add(transform.rotation);
+            trackedVelocity.Add(rb.linearVelocity);
+            count.Add(1);
+        }
     }
 
     void TestObj()
