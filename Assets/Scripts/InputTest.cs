4608a0094307bef9cfbd024f33510bcd0d141da9
Icey_Carp <Dixenbv@gmail.com>
2026-04-15 11:40:44 +0200

    GRABING
    Added grabing for the right hand. might be a bit buggy

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -1,24 +1,74 @@
+using System.Threading;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputTest : MonoBehaviour
 {
-    public Transform resetOrigin;
+    public InputActionProperty ActionButton;
 
-    public InputActionProperty testActionButton;
+    private GameObject touchingObject;
+    private GameObject heldObject;
+
+    [SerializeField] private Rigidbody HandRB;
 
     void Update()
     {
-        bool button = testActionButton.action.IsPressed();
+        bool button = ActionButton.action.IsPressed();
 
-        if (button)
+        if (button && touchingObject)
         {
-            ResetPosition(); 
-        } 
+            GrabObject();
+            Debug.Log("Grabbing object");
+        }
+
+        if (!button && heldObject)
+        {
+            ReleaseObject();
+            Debug.Log("releasing object");
+        }
     }
 
-    void ResetPosition()
+    void GrabObject()
     {
-        this.transform.position = resetOrigin.position;
+        heldObject = touchingObject;
+        if (GetComponent<FixedJoint>() == null)
+        {
+            var joint = AddFixedJoint();
+            joint.connectedBody = heldObject.GetComponent<Rigidbody>();
+        }
+    }
+
+    void ReleaseObject()
+    {
+        if (GetComponent<FixedJoint>())
+        {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            heldObject.GetComponent<Rigidbody>().linearVelocity = HandRB.linearVelocity;
+            heldObject.GetComponent<Rigidbody>().angularVelocity = HandRB.angularVelocity;
+        }
+        heldObject = null;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        touchingObject = other.gameObject;
+        Debug.Log("Got Object");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        touchingObject = null;
+        Debug.Log("Left Object");
+    }
+
+    FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        fx.massScale = 0f;
+        return fx;
     }
 }
